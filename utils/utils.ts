b1082d18e658d6e9e6ffa2c269b83e8a3121ab02
anhsayshello <anhle0347@gmail.com>
2025-10-17 16:58:41 +0700

    feat: add validate status, getToday functions

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,6 @@
+import type { AppointmentStatus } from "@prisma/client";
+import { format, getMonth } from "date-fns";
+
 export const getTokenFrom = (request) => {
   const authorization = request.get("authorization");
   if (authorization && authorization.startsWith("Bearer ")) {
@@ -5,3 +8,37 @@ export const getTokenFrom = (request) => {
   }
   return null;
 };
+
+export function isValidStatus(status: string): status is AppointmentStatus {
+  return ["PENDING", "SCHEDULED", "COMPLETED", "CANCELLED"].includes(status);
+}
+
+export const initializeMonthlyData = () => {
+  const this_year = new Date().getFullYear();
+
+  const months = Array.from(
+    { length: getMonth(new Date()) + 1 },
+    (_, index) => ({
+      month: format(new Date(this_year, index), "MMM"),
+      appointment: 0,
+      completed: 0,
+    })
+  );
+
+  return months;
+};
+
+export const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+export default function getToday() {
+  const today = new Date().getDay();
+
+  return weekday[today];
+}

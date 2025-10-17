64be01d11b17b5103bfdf8ae5e7d899bc8843a2b
anhsayshello <anhle0347@gmail.com>
2025-10-17 17:02:23 +0700

    refactor: catch auth error

--- middlewares/errorHandler.ts
+++ middlewares/errorHandler.ts
@@ -3,11 +3,14 @@ import AppError from "../utils/app-error";
 export default function errorHandler(error, _req, res, next) {
   if (error instanceof AppError || error.name === "AppError") {
     return res.status(error.status).json({ error: error.message });
-  } else if (
-    error.code === "auth/argument-error" ||
-    error.errorInfo?.code === "auth/argument-error"
-  ) {
+  } else if (error.code === "auth/argument-error") {
     return res.status(401).json({ error: "Invalid token" });
+  } else if (error.code === "auth/id-token-expired") {
+    return res.status(401).json({
+      // error: "Token expired",
+      // message: "Your session has expired. Please sign in again.",
+      error: "Your session has expired. Please sign in again.",
+    });
   }
 
   next(error);

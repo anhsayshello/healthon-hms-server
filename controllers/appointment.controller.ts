4d25889125785bd66162d825f7558b42a195ad65
anhsayshello <anhle0347@gmail.com>
2025-10-20 10:23:44 +0700

    feat: add create appointment service

--- controllers/appointment.controller.ts
+++ controllers/appointment.controller.ts
@@ -3,12 +3,32 @@ import { authMiddlewares } from "../middlewares";
 import appoitmentService from "../services/appointment.service";
 
 const appointmentRouter = Router();
+
 appointmentRouter.use(authMiddlewares);
 
 appointmentRouter.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await appoitmentService.getAppointmentById(Number(id));
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+appointmentRouter.post("/", async (req, res, next) => {
+  try {
+    const uid = req.uid;
+    const { doctor_id, type, appointment_date, time, note } = req.body;
+    console.log(req.body, "body");
+    const result = await appoitmentService.createNewAppointment(
+      uid as string,
+      doctor_id,
+      appointment_date,
+      time,
+      type,
+      note
+    );
     return res.status(200).json(result);
   } catch (error) {
     next(error);
--- services/appointment.service.ts
+++ services/appointment.service.ts
@@ -1,4 +1,4 @@
-import { Role, type AppointmentStatus } from "@prisma/client";
+import { AppointmentStatus, Role } from "@prisma/client";
 import type { Appointment } from "../types/appointment.type";
 import { getMonth, format, startOfYear, endOfMonth } from "date-fns";
 import { initializeMonthlyData, isValidStatus } from "../utils/utils";
@@ -81,28 +81,140 @@ const appoitmentService = {
     return { data };
   },
 
+  async createNewAppointment(
+    patient_id: string,
+    doctor_id: string,
+    appointment_date: string,
+    time: string,
+    type: string,
+    note?: string
+  ) {
+    const targetDate = new Date(appointment_date);
+    console.log(targetDate, "target");
+    const startOfDay = new Date(targetDate.setHours(0, 0, 0, 0));
+    const endOfDay = new Date(targetDate.setHours(23, 59, 59, 999));
+
+    const existingAppointment = await prisma.appointment.findFirst({
+      where: {
+        patient_id,
+        appointment_date: {
+          gte: startOfDay,
+          lte: endOfDay,
+        },
+        status: { not: AppointmentStatus.CANCELLED },
+      },
+    });
+
+    if (existingAppointment) {
+      throw new AppError(
+        `You already have an appointment on ${targetDate.toLocaleDateString()}.`
+      );
+    }
+
+    const doctor = await prisma.doctor.findUnique({
+      where: { uid: doctor_id },
+      select: { first_name: true, last_name: true, working_days: true },
+    });
+
+    const dayName = new Date(appointment_date).toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+
+    const isAvailable = doctor?.working_days.some((item) => {
+      console.log(appointment_date);
+      return item.day === dayName;
+    });
+
+    if (!isAvailable) {
+      throw new AppError(
+        `Please select a date within the doctor's working schedule.`
+      );
+    }
+
+    const newAppointment = await prisma.appointment.create({
+      data: {
+        patient_id,
+        doctor_id,
+        appointment_date,
+        time,
+        status: AppointmentStatus.PENDING,
+        type,
+        note: note ?? null,
+      },
+    });
+
+    return { data: newAppointment };
+  },
+
   async updateAppointmentById(
     uid: string,
     id: number,
     status: AppointmentStatus,
-    reason: string
+    reason?: string
   ) {
     const currentUser = await getAuth().getUser(uid);
-    const isAdmin = currentUser.customClaims?.role === Role.ADMIN;
-    if (!isAdmin) {
-      throw new AppError("You are not authorized to update appointments.", 403);
+    const role = currentUser.customClaims?.role;
+
+    if (!role) {
+      throw new AppError("User role not found", 403);
     }
 
-    const data = await prisma.appointment.update({
-      where: { id },
-      data: {
-        status: status,
-        reason: reason,
-      },
-    });
+    const appointment = await prisma.appointment.findUnique({ where: { id } });
+    if (!appointment) {
+      throw new AppError("Appointment not found", 404);
+    }
+    if (appointment.status === AppointmentStatus.COMPLETED) {
+      throw new AppError("This appointment has already been completed");
+    } else if (appointment.status === AppointmentStatus.CANCELLED) {
+      throw new AppError("This appointment has already been cancelled");
+    }
 
-    if (!data) {
-      throw new AppError("Appointment data not found", 404);
+    let data = null;
+
+    if (role === Role.PATIENT) {
+      if (appointment.patient_id !== uid) {
+        throw new AppError(
+          "You are not authorized to cancel this appointment.",
+          403
+        );
+      }
+
+      if (status !== AppointmentStatus.CANCELLED) {
+        throw new AppError("You can only cancel your appointments.", 400);
+      }
+
+      data = await prisma.appointment.update({
+        where: { id },
+        data: {
+          status: AppointmentStatus.CANCELLED,
+          reason: reason ?? null,
+        },
+      });
+    } else if (role === Role.DOCTOR) {
+      if (appointment.doctor_id !== uid) {
+        throw new AppError(
+          "You are not authorized to update this appointment.",
+          403
+        );
+      }
+
+      data = await prisma.appointment.update({
+        where: { id },
+        data: {
+          status,
+          reason: reason ?? null,
+        },
+      });
+    } else if (role === Role.ADMIN) {
+      data = await prisma.appointment.update({
+        where: { id },
+        data: {
+          status,
+          reason: reason ?? null,
+        },
+      });
+    } else {
+      throw new AppError("You are not authorized to update appointments.", 403);
     }
 
     return { data };

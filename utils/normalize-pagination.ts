d13ec9cf7f2ca837a97d0819067564968e0ddd72
anhsayshello <anhle0347@gmail.com>
2025-11-04 09:40:18 +0700

    refactor: adjust params type

--- utils/normalize-pagination.ts
+++ utils/normalize-pagination.ts
@@ -1,6 +1,6 @@
-export default function normalizePagination(page?: number, limit?: number) {
-  const PAGENUMBER = !page || page <= 0 ? 1 : page;
-  const LIMIT = limit || 10;
+export default function normalizePagination(page?: string, limit?: string) {
+  const PAGENUMBER = !page || Number(page) <= 0 ? 1 : Number(page);
+  const LIMIT = Number(limit) || 10;
   const SKIP = (PAGENUMBER - 1) * LIMIT;
 
   return { PAGENUMBER, LIMIT, SKIP };

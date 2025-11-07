78610f0105ec68537639ace42117661665fe3369
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:27:30 +0700

    ✨ feat(lab): add service to retrieve lab services

---
+++ services/lab/getLabServices.service.ts
@@ -0,0 +1,26 @@
+import prisma from "../../config/db";
+import type { SearchQueryParams } from "../../types";
+import normalizePagination from "../../utils/normalize-pagination";
+
+export default async function getLabServices(params: SearchQueryParams) {
+  const { page, limit } = params;
+  const { PAGENUMBER, SKIP, LIMIT } = normalizePagination(page, limit);
+
+  const [services, totalRecords] = await Promise.all([
+    prisma.service.findMany({
+      orderBy: { created_at: "asc" },
+      skip: SKIP,
+      take: LIMIT,
+    }),
+    prisma.service.count({}),
+  ]);
+
+  const totalPages = Math.ceil(totalRecords / LIMIT);
+
+  return {
+    data: services,
+    totalPages,
+    currentPage: PAGENUMBER,
+    totalRecords,
+  };
+}

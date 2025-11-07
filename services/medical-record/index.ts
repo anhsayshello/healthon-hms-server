f8a5e6d6520d77ec77eb12abd01fd8e0614161c7
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:29:40 +0700

    ✨ feat(medical-record): add new medical record retrieval services

--- services/medical-record/index.ts
+++ services/medical-record/index.ts
@@ -1,7 +1,11 @@
+import getMedicalRecordById from "./getMedicalRecordById.service";
 import getMedicalRecords from "./getMedicalRecords.service";
+import getTodayMedicalRecords from "./getTodayMedicalRecords.service";
 
 const medicalRecordService = {
   getMedicalRecords,
+  getTodayMedicalRecords,
+  getMedicalRecordById,
 };
 
 export default medicalRecordService;

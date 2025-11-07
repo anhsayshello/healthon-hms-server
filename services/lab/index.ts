80bc24e3e5bdda89fb391baf39a27838e269b990
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:28:45 +0700

    ✨ feat(lab): introduce lab service module

---
+++ services/lab/index.ts
@@ -0,0 +1,21 @@
+import cancelLabRequest from "./cancelLabRequest.service";
+import createLabRequest from "./createLabRequest.service";
+import createLabService from "./createLabService.service";
+import finishLabTest from "./finishLabTest.service";
+import getLabServices from "./getLabServices.service";
+import getLabTestById from "./getLabTestById.service";
+import getLabTestRequests from "./getLabTestRequests.service";
+import startLabTest from "./startLabTest.service";
+
+const labService = {
+  createLabService,
+  getLabServices,
+  createLabRequest,
+  cancelLabRequest,
+  getLabTestRequests,
+  startLabTest,
+  finishLabTest,
+  getLabTestById,
+};
+
+export default labService;

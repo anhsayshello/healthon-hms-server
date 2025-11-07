dba10fb6a430b085aec1a8381e7d09ad74bc6e2d
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:23:48 +0700

    ✨ feat(medicalRecord): add endpoints for record access

--- controllers/medicalRecord.controller.ts
+++ controllers/medicalRecord.controller.ts
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import { appCheckVerification } from "../middlewares/appCheckVerification";
-import authService from "../services/auth/auth.service";
 import { authMiddlewares } from "../middlewares";
 import medicalRecordService from "../services/medical-record";
 import type { SearchQueryParams } from "../types";
@@ -13,6 +11,30 @@ medicalRecordRouter.get("/", async (req, res, next) => {
   try {
     const params: SearchQueryParams = req.query;
     const result = await medicalRecordService.getMedicalRecords(params);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+medicalRecordRouter.get("/today", async (req, res, next) => {
+  try {
+    const params: SearchQueryParams = req.query;
+    const uid = req?.uid as string;
+    const result = await medicalRecordService.getTodayMedicalRecords(
+      uid,
+      params
+    );
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+medicalRecordRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await medicalRecordService.getMedicalRecordById(Number(id));
     return res.status(200).json(result);
   } catch (error) {
     next(error);

7ec9b84dbd6adc4907ca969c53d90fba98e6a24d
anhsayshello <anhle0347@gmail.com>
2025-11-14 23:17:17 +0700

    chore(build): remove obsolete prisma migrations

--- prisma/migrations/20251107043040_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[service_id,medical_id]` on the table `LabTest` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "LabTest_service_id_medical_id_key" ON "public"."LabTest"("service_id", "medical_id");
--- prisma/migrations/20251111160709_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Medication" ADD COLUMN     "currency" "public"."Currency" NOT NULL DEFAULT 'VND';
